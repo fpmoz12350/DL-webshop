789a2e2070e8c799b1db992a47f3c68e3637f20c
fpmoz12355 <lucijaz98@gmail.com>
2021-01-13 09:00:02 +0100

    frontend commit 1

--- resources/views/webshop/template/footer.blade.php
+++ resources/views/webshop/template/footer.blade.php
@@ -1,6 +1,55 @@
-<footer class="py-4 bg-dark">
-    <div class="container">
-      <p class="m-0 text-center text-white">linkovi na viziju, github, face, itd u obliku ikona</p>
+
+
+    <!----------- Footer ------------>
+    <footer class="footer-bs footer-dark bg-dark">
+
+
+    <div class="row w-50 p-3">
+    
     </div>
-    <!-- /.container -->
-  </footer>
+    
+    <div class="row">
+
+
+    <div class="col-sm-1">
+    
+            
+    </div>
+
+    <div class="col-sm">
+    <class="nav-item">
+            <a class="nav-link text-light" href="about.html">Terms & Conditions</a>
+    </div>
+    <div class="col-sm">
+    
+    <class="nav-item">
+            <a class="nav-link text-light" a href="https://drive.google.com/file/d/1UrYAwmVm7vzIOv6Y6Kd0VJnS0OFNDzG5/view" target="_blank" >Vizija</a>
+
+    </div>
+    <div class="col-sm">
+    <class="nav-item">
+            <a class="nav-link text-light" a href="https://g3-zadaca.000webhostapp.com" target="_blank"  >O nama</a>
+    </div>
+    <div class="col-sm">
+    <class="nav-item">
+            <a class="nav-link text-light" href="about.html">Kontakt</a>
+    </div>
+    
+  </div>
+  <div class="row w-50 p-3">
+    
+    </div> 
+
+    <i class="fab fa-github"></i>
+
+
+  <div class="row">
+  </hr>
+
+    <section style="text-align:center; margin:10px auto; color:white"><p>© Designed by Dario & Lucija </a></p></section>
+    </div>
+
+    </footer>
+  
+
+</div>
--- resources/views/webshop/template/header.blade.php
+++ resources/views/webshop/template/header.blade.php
@@ -4,29 +4,41 @@
         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
       </ol>
       <div class="carousel-inner" role="listbox">
         <!-- Slide One - Set the background image for this slide in the line below -->
-        <div class="carousel-item active" style="background-image: url('http://placehold.it/1900x1080')">
+        <div class="carousel-item active" style="background-image: url('https://www.hm.com/nt-north/uploads/2021/01/1053A-3x2-1-1280x853.jpg')">
           <div class="carousel-caption d-none d-md-block">
-            <h3>First Slide</h3>
-            <p>This is a description for the first slide.</p>
+            <h3>"NOVO NORMALNO"?</h3>
+            <p>Tko kaže da ne možete izgledati i osjećati se dobro i dok radite kod kuće?</p>
           </div>
         </div>
         <!-- Slide Two - Set the background image for this slide in the line below -->
-        <div class="carousel-item" style="background-image: url('http://placehold.it/1900x1080')">
+        <div class="carousel-item" style="background-image: url('https://www.hm.com/nt-north/uploads/2020/12/3043-the-cult-classics-3x2-1-1280x853.jpg')">
           <div class="carousel-caption d-none d-md-block">
-            <h3>Second Slide</h3>
-            <p>This is a description for the second slide.</p>
+            <h3>KULTNI KLASICI</h3>
+            <p>USKORO: nova proljetna kolekcija jakni i hoodica sa likovima iz filmskih klasika.</p>
           </div>
         </div>
         <!-- Slide Three - Set the background image for this slide in the line below -->
-        <div class="carousel-item" style="background-image: url('http://placehold.it/1900x1080')">
+        <div class="carousel-item" style="background-image: url('https://31ns9t3tahh91zt99bieate1-wpengine.netdna-ssl.com/wp-content/uploads/2021/01/B2F2.jpg')">
           <div class="carousel-caption d-none d-md-block">
-            <h3>Third Slide</h3>
-            <p>This is a description for the third slide.</p>
+            <h3>AQUA kolekcija</h3>
+            <p>Od plastičnih boca - do hoodica. </p>
+            <p>Nova kolekcija od recikliranih materijala </p>
           </div>
         </div>
+     
+      <!-- Slide Four - Set the background image for this slide in the line below -->
+      <div class="carousel-item" style="background-image: url('https://i1.wp.com/metro.co.uk/wp-content/uploads/2020/01/PRC_119233643.jpg?quality=90&strip=all&zoom=1&resize=644%2C338&ssl=1')">
+          <div class="carousel-caption d-none d-md-block">
+            <h3>ECO SPORTWEAR</h3>
+            <p>Nova sportska kolekcija od recikliranog poliestera. </p>
+            <p>ECO is the new black.</p>
+          </div>
+        </div>
+      </div>
       </div>
       <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -37,4 +49,6 @@
         <span class="sr-only">Next</span>
       </a>
     </div>
+    
+    
   </header>
