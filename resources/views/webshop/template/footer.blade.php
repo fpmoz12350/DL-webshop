4e87bef3b808ff3cc26f4105a4239cd4e138a5d7
fpmoz12350 <dario.damjanovic@hotmail.com>
2021-01-20 22:53:17 +0100

    peti commit

--- resources/views/webshop/template/footer.blade.php
+++ resources/views/webshop/template/footer.blade.php
@@ -1,6 +1,55 @@
-<footer class="py-4 bg-dark">
-    <div class="container">
-      <p class="m-0 text-center text-white">linkovi na viziju, github, face, itd u obliku ikona</p>
+
+
+    <!----------- Footer ------------>
+    <footer class="footer-bs footer-dark bg-dark">
+
+
+        <div class="row w-50 p-3">
+        
+        </div>
+        
+        <div class="row">
+    
+    
+        <div class="col-sm-1">
+        
+                
+        </div>
+    
+        <div class="col-sm">
+        <div class="nav-item">
+                <a class="nav-link text-light" href="{{ route('terms') }}">Terms & Conditions</a>
+        </div>
+        <div class="col-sm">
+        
+        <div class="nav-item">
+                <a class="nav-link text-light" a href="{{ route('faq') }}">FAQ</a>
+    
+        </div>
+        <div class="col-sm">
+        <div class="nav-item">
+                <a class="nav-link text-light" a href="{{ route('about_us') }}">O nama</a>
+        </div>
+        <div class="col-sm">
+        <div class="nav-item">
+                <a class="nav-link text-light" href="{{ route('contact') }}">Kontakt</a>
+        </div>
+        
+      </div>
+      <div class="row w-50 p-3">
+        
+        </div> 
+    
+        <i class="fab fa-github"></i>
+    
+    
+      <div class="row">
+      <hr/>
+    
+        <section style="text-align:center; margin:10px auto; color:white"><p>© Designed by Dario & Lucija </a></p></section>
+        </div>
+    
+        </footer>
+      
+    
     </div>
-    <!-- /.container -->
-  </footer>
